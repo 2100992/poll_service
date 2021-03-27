b66b3ef53caf7d5cda05a2b7da4f90477e82bbfb
Shvd <2100992@gmail.com>
2021-03-27 07:25:57 +0300

    add admin page

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from polls import models
+
+
+@admin.register(models.Poll)
+class PollAdmin(admin.ModelAdmin):
+    list_per_page = 20
+    # list_display =
+
+
+@admin.register(models.Query)
+class QueryAdmin(admin.ModelAdmin):
+    list_per_page = 20
+
+
+@admin.register(models.Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_per_page = 20
+
+
+@admin.register(models.Response)
+class ResponseAdmin(admin.ModelAdmin):
+    list_per_page = 20
